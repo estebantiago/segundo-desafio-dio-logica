3b0a69716af712d4c54db98067c4895e67321ad9
estebantiago <estebantiago22@gmail.com>
2024-05-15 13:01:58 -0300

    segundo commit

---
+++ index.js
@@ -0,0 +1,40 @@
+let resultado = calcularSaldo(120, 10)
+
+
+console.log(`O Herói tem de saldo de ${resultado} está no nível de ${classificarHeroi()} `) ;
+
+
+
+
+function calcularSaldo(vitoria, derrota){
+
+    let resultado = vitoria - derrota;
+    return resultado;
+    
+    
+}        
+function classificarHeroi(){
+   if (resultado <= 10){
+     return ' Ferro'
+   
+    }  else if(resultado >= 11 && resultado <= 20 ) {
+            return ' Bronze'
+
+}      else if (resultado >= 21 && resultado <= 50 ){
+            return ' Prata'
+
+}      else if (resultado >= 51 && resultado <= 80 ){
+            return ' Ouro'
+
+}      else if (resultado >= 81 && resultado <= 90){
+            return ' Diamante'
+
+}      else if (resultado >= 91 && resultado <= 100){
+            return ' Lendário'
+
+}      else {
+            return ' Imortal'  
+}
+
+        
+}
